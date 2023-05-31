6ed29e8af04edc71deeab3db056cb9d220b7bd54
mitchell <70453897+orbitalquark@users.noreply.github.com>
2023-05-31 19:11:31 -0400

    Reformat source code to use tabs for indentation. Requires some bugfixes to LuaFormatter's handling of tabs.

--- init.lua
+++ init.lua
@@ -21,30 +21,30 @@ local M = {}
 -- Localizations.
 local _L = _L
 if not rawget(_L, 'Reformat') then
-  -- Menu.
-  _L['Reformat'] = 'Reformat'
-  _L['Code'] = '_Code'
-  _L['Paragraph'] = '_Paragraph'
+	-- Menu.
+	_L['Reformat'] = 'Reformat'
+	_L['Code'] = '_Code'
+	_L['Paragraph'] = '_Paragraph'
 end
 
 --- Helper function that returns whether or not the given config file exists in the current or
 -- a parent directory of the current buffer's filename.
 local function has_config_file(filename)
-  if not buffer.filename then return false end
-  local dir = buffer.filename:match('^(.+)[/\\]')
-  while dir do
-    if lfs.attributes(dir .. '/' .. filename) then return true end
-    dir = dir:match('^(.+)[/\\]')
-  end
-  return false
+	if not buffer.filename then return false end
+	local dir = buffer.filename:match('^(.+)[/\\]')
+	while dir do
+		if lfs.attributes(dir .. '/' .. filename) then return true end
+		dir = dir:match('^(.+)[/\\]')
+	end
+	return false
 end
 
 --- Map of lexer languages to string code formatter commands or functions that return such
 -- commands.
 M.commands = {
-  lua = function() return has_config_file('.lua-format') and 'lua-format' or nil end,
-  cpp = function() return has_config_file('.clang-format') and 'clang-format -style=file' or nil end,
-  go = 'gofmt'
+	lua = function() return has_config_file('.lua-format') and 'lua-format' or nil end,
+	cpp = function() return has_config_file('.clang-format') and 'clang-format -style=file' or nil end,
+	go = 'gofmt'
 }
 M.commands.ansi_c = M.commands.cpp
 
@@ -72,21 +72,21 @@ M.line_length = 100
 -- text or the current paragraph, according to the rules of `textadept.editing.filter_through()`.
 -- @see commands
 function M.code()
-  local command = M.commands[buffer.lexer_language]
-  if type(command) == 'function' then command = command() end
-  if not command then return end
-  local current_dir = lfs.currentdir()
-  local dir = (buffer.filename or ''):match('^(.+)[/\\]') or io.get_project_root()
-  if dir and dir ~= current_dir then lfs.chdir(dir) end
-  textadept.editing.filter_through(command)
-  if dir and dir ~= current_dir then lfs.chdir(current_dir) end -- restore
+	local command = M.commands[buffer.lexer_language]
+	if type(command) == 'function' then command = command() end
+	if not command then return end
+	local current_dir = lfs.currentdir()
+	local dir = (buffer.filename or ''):match('^(.+)[/\\]') or io.get_project_root()
+	if dir and dir ~= current_dir then lfs.chdir(dir) end
+	textadept.editing.filter_through(command)
+	if dir and dir ~= current_dir then lfs.chdir(current_dir) end -- restore
 end
 events.connect(events.FILE_BEFORE_SAVE, function(filename)
-  if not M.on_save then return end
-  if filename then
-    for _, patt in ipairs(M.ignore_file_patterns) do if filename:find(patt) then return end end
-  end
-  M.code()
+	if not M.on_save then return end
+	if filename then
+		for _, patt in ipairs(M.ignore_file_patterns) do if filename:find(patt) then return end end
+	end
+	M.code()
 end)
 
 --- Reformats using the Unix `fmt` tool either the selected text or the current paragraph,
@@ -98,47 +98,47 @@ end)
 -- it is not reformatted.
 -- @see line_length
 function M.paragraph()
-  if buffer.selection_empty then
-    local s = buffer:line_from_position(buffer.current_pos)
-    local style = buffer.style_at[buffer.line_indent_position[s]]
-    local e = s + 1
-    for i = s - 1, 1, -1 do
-      if buffer.style_at[buffer.line_indent_position[i]] ~= style then break end
-      s = s - 1
-    end
-    local line = buffer:get_line(s)
-    for _, header in ipairs(M.ignore_header_lines) do
-      if line:find('^%s*' .. header:gsub('%p', '%%%0') .. '%s*$') then
-        s = s + 1
-        break
-      end
-    end
-    for i = e, buffer.line_count do
-      if buffer.style_at[buffer.line_indent_position[i]] ~= style then break end
-      e = e + 1
-    end
-    line = buffer:get_line(e - 1)
-    for _, footer in ipairs(M.ignore_footer_lines) do
-      if line:find('^%s*' .. footer:gsub('%p', '%%%0')) then
-        e = e - 1
-        break
-      end
-    end
-    buffer:set_sel(buffer:position_from_line(s), buffer:position_from_line(e))
-  end
-  local prefix = buffer:get_line(buffer:line_from_position(buffer.selection_start)):match(
-    '^%s*(%p*)')
-  local cmd = 'fmt -w ' .. M.line_length .. ' -c'
-  if prefix ~= '' then cmd = string.format('%s -p "%s"', cmd, prefix) end
-  textadept.editing.filter_through(cmd)
+	if buffer.selection_empty then
+		local s = buffer:line_from_position(buffer.current_pos)
+		local style = buffer.style_at[buffer.line_indent_position[s]]
+		local e = s + 1
+		for i = s - 1, 1, -1 do
+			if buffer.style_at[buffer.line_indent_position[i]] ~= style then break end
+			s = s - 1
+		end
+		local line = buffer:get_line(s)
+		for _, header in ipairs(M.ignore_header_lines) do
+			if line:find('^%s*' .. header:gsub('%p', '%%%0') .. '%s*$') then
+				s = s + 1
+				break
+			end
+		end
+		for i = e, buffer.line_count do
+			if buffer.style_at[buffer.line_indent_position[i]] ~= style then break end
+			e = e + 1
+		end
+		line = buffer:get_line(e - 1)
+		for _, footer in ipairs(M.ignore_footer_lines) do
+			if line:find('^%s*' .. footer:gsub('%p', '%%%0')) then
+				e = e - 1
+				break
+			end
+		end
+		buffer:set_sel(buffer:position_from_line(s), buffer:position_from_line(e))
+	end
+	local prefix = buffer:get_line(buffer:line_from_position(buffer.selection_start)):match(
+		'^%s*(%p*)')
+	local cmd = 'fmt -w ' .. M.line_length .. ' -c'
+	if prefix ~= '' then cmd = string.format('%s -p "%s"', cmd, prefix) end
+	textadept.editing.filter_through(cmd)
 end
 
 -- Add menu entry.
 local m_edit = textadept.menu.menubar['Edit']
 table.insert(m_edit, #m_edit - 1, {
-  title = _L['Reformat'], --
-  {_L['Code'], M.code}, --
-  {_L['Paragraph'], M.paragraph}
+	title = _L['Reformat'], --
+	{_L['Code'], M.code}, --
+	{_L['Paragraph'], M.paragraph}
 })
 keys[not OSX and 'ctrl+J' or 'cmd+J'] = M.paragraph
 
