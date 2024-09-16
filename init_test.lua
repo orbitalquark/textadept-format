4c98163cdd47723b67de62923d56b3dabbdb4029
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2024-09-16 16:59:15 -0400

    Fixed clang-format check in tests.

--- init_test.lua
+++ init_test.lua
@@ -2,7 +2,7 @@
 
 local format = require('format')
 
-local have_clang_format = LINUX or OSX and os.getenv('CI') == ''
+local have_clang_format = LINUX or OSX and not os.getenv('CI')
 
 test('format.code should use clang-format if a .clang-format exists', function()
 	local file = 'file.c'
@@ -16,7 +16,7 @@ test('format.code should use clang-format if a .clang-format exists', function()
 
 	test.assert_equal(buffer:get_text(), 'int main() { return 0; }')
 end)
-if have_clang_format then skip('clang-format is not available') end
+if not have_clang_format then skip('clang-format is not available') end
 
 test('format.code should not format on save if format.on_save is disabled', function()
 	local _<close> = test.mock(format, 'on_save', false)
@@ -30,7 +30,7 @@ test('format.code should not format on save if format.on_save is disabled', func
 
 	test.assert_equal(buffer:get_text(), code)
 end)
-if have_clang_format then skip('clang-format is not available') end
+if not have_clang_format then skip('clang-format is not available') end
 
 test('format.code should ignore saving files matching format.ignore_file_patterns', function()
 	local subdir = 'subdir'
@@ -47,7 +47,7 @@ test('format.code should ignore saving files matching format.ignore_file_pattern
 
 	test.assert_equal(buffer:get_text(), code)
 end)
-if have_clang_format then skip('clang-format is not available') end
+if not have_clang_format then skip('clang-format is not available') end
 
 test('format.paragraph should reformat the current paragraph', function()
 	local _<close> = test.mock(format, 'line_length', 10)
@@ -162,7 +162,7 @@ test('format.code should find files like .clang-format in subdirectories', funct
 
 	test.assert_equal(buffer:get_text(), test.lines{'int main() { return 0; }', ''})
 end)
-if have_clang_format then skip('clang-format is not available') end
+if not have_clang_format then skip('clang-format is not available') end
 
 test('format.code should not format without a file like .clang-format', function()
 	local file = 'file.c'
